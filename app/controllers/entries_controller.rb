bd3dbdd9910dd239506d19cc5678d23b2a9e39e4
Abhinag Naidu <abhinag.naidu@kellogg.northwestern.edu>
2024-03-10 05:18:16 +0000

    Initial commit

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,13 +4,17 @@ class EntriesController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "id" => session["user_id"] })
+    if @user != nil
     @entry = Entry.new
     @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
     @entry["occurred_on"] = params["occurred_on"]
+    @entry.uploaded_image.attach(params["uploaded_image"])
+    @entry["description"] = params["description"]
     @entry["place_id"] = params["place_id"]
+    @entry["user_id"] = session["user_id"]
     @entry.save
     redirect_to "/places/#{@entry["place_id"]}"
   end
-
+end
 end
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,17 +6,22 @@ class PlacesController < ApplicationController
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"] })
+    # filter entries by place and logged-in user
+    @entries = Entry.where({ "place_id" => @place["id"], "user_id" => session["user_id"]})
   end
 
   def new
   end
 
   def create
+    @user = User.find_by({ "id" => session["user_id"] })
+    if @user != nil
     @place = Place.new
     @place["name"] = params["name"]
     @place.save
+  else
+    flash["notice"] = "Login first."
+  end
     redirect_to "/places"
   end
-
 end
