51224e69c6f4ef3be15f17f76fbcd3dabfbfd693
Abhinag Naidu <abhinag.naidu@kellogg.northwestern.edu>
2024-03-10 21:36:52 +0000

    Image control code added

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -6,15 +6,15 @@ class EntriesController < ApplicationController
   def create
     @user = User.find_by({ "id" => session["user_id"] })
     if @user != nil
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry.uploaded_image.attach(params["uploaded_image"])
-    @entry["description"] = params["description"]
-    @entry["place_id"] = params["place_id"]
-    @entry["user_id"] = session["user_id"]
-    @entry.save
-    redirect_to "/places/#{@entry["place_id"]}"
+      @entry = Entry.new
+      @entry["title"] = params["title"]
+      @entry["occurred_on"] = params["occurred_on"]
+      @entry.uploaded_image.attach(params["uploaded_image"])
+      @entry["description"] = params["description"]
+      @entry["place_id"] = params["place_id"]
+      @entry["user_id"] = session["user_id"]
+      @entry.save
+      redirect_to "/places/#{@entry["place_id"]}"
+    end
   end
-end
 end
